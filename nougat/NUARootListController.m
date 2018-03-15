514cd6c252023f103f33bb51855dab192f34e7ab
Shade-Zepheri <ziroalpha@gmail.com>
2018-03-14 22:41:50 -0500

    [prefs] New color scheme

--- nougat/NUARootListController.m
+++ nougat/NUARootListController.m
@@ -1,9 +1,30 @@
-#include "NUARootListController.h"
+#import "NUARootListController.h"
+#import <CepheiPrefs/HBAppearanceSettings.h>
 
 @implementation NUARootListController
 
+#pragma mark - HBListController
+
 + (NSString *)hb_specifierPlist {
     return @"Root";
+}
+
+#pragma mark - Initializer
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        HBAppearanceSettings *appearanceSettings = [[HBAppearanceSettings alloc] init];
+        appearanceSettings.tintColor = [UIColor colorWithRed:0.32 green:0.18 blue:0.66 alpha:1.0];
+        appearanceSettings.navigationBarTintColor = [UIColor whiteColor];
+        appearanceSettings.navigationBarTitleColor = [UIColor whiteColor];
+        appearanceSettings.navigationBarBackgroundColor = [UIColor colorWithRed:0.32 green:0.18 blue:0.66 alpha:1.0];
+        appearanceSettings.statusBarTintColor = [UIColor whiteColor];
+        appearanceSettings.translucentNavigationBar = NO;
+        self.hb_appearanceSettings = appearanceSettings;
+    }
+
+    return self;
 }
 
 @end
