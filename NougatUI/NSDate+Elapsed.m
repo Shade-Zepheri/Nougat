6ab5e8b21bcc4de200bc3f61f5f99670e2dc5eaf
Shade-Zepheri <ziroalpha@gmail.com>
2020-03-26 20:52:45 -0500

    [UI] Add localizations

--- NougatUI/NSDate+Elapsed.m
+++ NougatUI/NSDate+Elapsed.m
@@ -1,26 +1,31 @@
 #import "NSDate+Elapsed.h"
+#import <HBLog.h>
 
 @implementation NSDate (Elapsed)
 
 - (NSString *)getElapsedTime {
     NSDateComponents *dateComponents = [[NSCalendar currentCalendar] components:NSCalendarUnitDay | NSCalendarUnitHour | NSCalendarUnitMinute fromDate:self toDate:[NSDate date] options:0];
 
+    NSBundle *bundle = [NSBundle bundleForClass:[self class]];
     NSInteger days = dateComponents.day;
     if (days > 0) {
-        return [NSString stringWithFormat:@"%zdd", days];
+        NSString *baseFormat = [bundle localizedStringForKey:@"RELATIVE_DATE_PAST_DAYS" value:@"%zdd" table:@"Localizable"];
+        return [NSString stringWithFormat:baseFormat, days];
     }
 
     NSInteger hours = dateComponents.hour;
     if (hours > 0) {
-        return [NSString stringWithFormat:@"%zdh", hours];
+        NSString *baseFormat = [bundle localizedStringForKey:@"RELATIVE_DATE_PAST_HOURS" value:@"%zdh" table:@"Localizable"];
+        return [NSString stringWithFormat:baseFormat, hours];
     }
 
     NSInteger minutes = dateComponents.minute;
     if (minutes > 0) {
-        return [NSString stringWithFormat:@"%zdm", minutes];
+        NSString *baseFormat = [bundle localizedStringForKey:@"RELATIVE_DATE_PAST_MINUTES" value:@"%zdm" table:@"Localizable"];
+        return [NSString stringWithFormat:baseFormat, minutes];
     } 
 
-    return @"now";
+    return [bundle localizedStringForKey:@"RELATIVE_DATE_PAST_SECONDS" value:@"now" table:@"Localizable"];
 }
 
 @end
