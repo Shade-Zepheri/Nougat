1ec5cb407465881b89563532b462edcdd84ea540
Shade-Zepheri <ziroalpha@gmail.com>
2020-03-31 18:33:25 -0500

    [UI] Fixed date label localization

--- NougatUI/NSDate+Elapsed.m
+++ NougatUI/NSDate+Elapsed.m
@@ -6,26 +6,26 @@
 - (NSString *)getElapsedTime {
     NSDateComponents *dateComponents = [[NSCalendar currentCalendar] components:NSCalendarUnitDay | NSCalendarUnitHour | NSCalendarUnitMinute fromDate:self toDate:[NSDate date] options:0];
 
-    NSBundle *bundle = [NSBundle bundleForClass:[self class]];
+    NSBundle *localizationBundle = [NSBundle bundleWithIdentifier:@"com.shade.NougatUI"];
     NSInteger days = dateComponents.day;
     if (days > 0) {
-        NSString *baseFormat = [bundle localizedStringForKey:@"RELATIVE_DATE_PAST_DAYS" value:@"%zdd" table:@"Localizable"];
+        NSString *baseFormat = [localizationBundle localizedStringForKey:@"RELATIVE_DATE_PAST_DAYS" value:@"%zdd" table:@"Localizable"];
         return [NSString stringWithFormat:baseFormat, days];
     }
 
     NSInteger hours = dateComponents.hour;
     if (hours > 0) {
-        NSString *baseFormat = [bundle localizedStringForKey:@"RELATIVE_DATE_PAST_HOURS" value:@"%zdh" table:@"Localizable"];
+        NSString *baseFormat = [localizationBundle localizedStringForKey:@"RELATIVE_DATE_PAST_HOURS" value:@"%zdh" table:@"Localizable"];
         return [NSString stringWithFormat:baseFormat, hours];
     }
 
     NSInteger minutes = dateComponents.minute;
     if (minutes > 0) {
-        NSString *baseFormat = [bundle localizedStringForKey:@"RELATIVE_DATE_PAST_MINUTES" value:@"%zdm" table:@"Localizable"];
+        NSString *baseFormat = [localizationBundle localizedStringForKey:@"RELATIVE_DATE_PAST_MINUTES" value:@"%zdm" table:@"Localizable"];
         return [NSString stringWithFormat:baseFormat, minutes];
     } 
 
-    return [bundle localizedStringForKey:@"RELATIVE_DATE_PAST_SECONDS" value:@"now" table:@"Localizable"];
+    return [localizationBundle localizedStringForKey:@"RELATIVE_DATE_PAST_SECONDS" value:@"now" table:@"Localizable"];
 }
 
 @end
